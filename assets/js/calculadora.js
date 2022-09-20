72d0900d53899700f6edc4c3681bbb5483484039
Joao Roccella <joao@rsti.info>
2022-09-20 00:53:37 +0000

    Main

---
+++ assets/js/calculadora.js
@@ -0,0 +1,5 @@
+function somaDigitos(digito1, digito2) {
+  const d1 = document.getElementById("digito1");
+  const d2 = document.getElementById("digito2");
+  document.getElementById("result-sum").innerHTML = d1 + d2;
+}
