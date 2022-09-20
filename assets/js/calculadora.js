b1fb5744b2e4faae6470b0b00c0eff92c2f5d317
Joao Roccella <joao@rsti.info>
2022-09-20 00:59:55 +0000

    Main

--- assets/js/calculadora.js
+++ assets/js/calculadora.js
@@ -1,4 +1,4 @@
-function somaDigitos(digito1, digito2) {
+function somaDigitos() {
   const d1 = document.getElementById("digito1");
   const d2 = document.getElementById("digito2");
   document.getElementById("result-sum").innerHTML = d1 + d2;
