72948d6486b19610791e1bb8685c7e397aa1cf14
Joao Roccella <joao@rsti.info>
2022-09-20 02:09:44 +0000

    Main Correções

--- assets/js/calculadora.js
+++ assets/js/calculadora.js
@@ -1,5 +1,5 @@
 function somaDigitos() {
   const d1 = parseFloat(document.getElementById("digito1").value);
   const d2 = parseFloat(document.getElementById("digito2").value);
-  document.getElementById("result-sum").innerHTML = parseFloat(d1 + d2).toFixed(2);
+  document.getElementById("result-sum").innerHTML = parseFloat(d1 + d2).toFixed(6);
 }
